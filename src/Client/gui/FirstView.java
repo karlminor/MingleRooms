d3e1d9cd3d0c089e833ae4b0598d8187575563c2
Robin <robin.robbe96@gmail.com>
2018-02-18 13:23:35 +0100

    FirstView will soon support selection of avatar

--- src/Client/gui/FirstView.java
+++ src/Client/gui/FirstView.java
@@ -1,11 +1,16 @@
 package Client.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.net.InetAddress;
@@ -13,7 +18,7 @@ import java.net.UnknownHostException;
 
 public class FirstView extends VBox{
     // Size for this view only
-    public static final int WIDTH = 400;
+    public static final int WIDTH = 500;
     public static final int HEIGHT = 300;
 
     private final int INSETS = 10;
@@ -38,19 +43,67 @@ public class FirstView extends VBox{
     }
 
     private void initComponents() {
+
+        HBox splitPanel = new HBox();
+        splitPanel.setAlignment(Pos.CENTER);
+        splitPanel.setSpacing(SPACING);
+
+        // Input fields
+        VBox inputFieldsPanel = new VBox();
+        inputFieldsPanel.setPrefWidth(WIDTH * 0.45);
+        inputFieldsPanel.setSpacing(SPACING);
+
+        HBox titleCenter = new HBox();
+        titleCenter.setAlignment(Pos.CENTER);
         Label title = new Label("Mingle Rooms - Client");
+        titleCenter.getChildren().add(title);
+
         inetAddressTF = new PPTextField("Internet address");
         portTF = new PPTextField("Port");
         nicknameTF = new PPTextField("Nickname");
-        Button connectB = new Button("Connect");
-        connectB.setOnAction(new ConnectButtonHandler());
 
-        // TODO temp values
+        HBox buttonCenter = new HBox();
+        buttonCenter.setAlignment(Pos.CENTER);
+        Button connectB = new Button("Connect");
+        connectB.setPrefWidth(WIDTH * 0.20);
+        connectB.setOnAction(new ConnectButtonHandler());
+        buttonCenter.getChildren().add(connectB);
+
+        // TODO temp values (can be removed)
         inetAddressTF.setText("192.168.0.1");
         portTF.setText("30000");
         nicknameTF.setText("SampleNickname");
 
-        getChildren().addAll(title, inetAddressTF, portTF, nicknameTF, connectB);
+        inputFieldsPanel.getChildren().addAll(titleCenter, inetAddressTF, portTF, nicknameTF, buttonCenter);
+
+        // Avatar selection
+        VBox sidePanel = new VBox();
+        sidePanel.setSpacing(SPACING);
+
+        HBox innerSideSplitPanel = new HBox();
+        innerSideSplitPanel.setSpacing(SPACING);
+
+        HBox avatarCenter = new HBox();
+        avatarCenter.setAlignment(Pos.CENTER);
+        Label avatar = new Label("Avatar");
+        avatarCenter.getChildren().add(avatar);
+
+        ObservableList<String> avatars = FXCollections.observableArrayList("Avatar 1", "Avatar 2", "Avatar 3", "Avatar 4");
+        ChoiceBox<String> avatarSelection = new ChoiceBox<>(avatars);
+        avatarSelection.getSelectionModel().selectFirst();
+
+        ImageView avatarIV = new ImageView("test.jpg");
+        avatarIV.setFitWidth(WIDTH * 0.2);
+        avatarIV.setPreserveRatio(true);
+        avatarIV.setSmooth(true);
+        avatarIV.setCache(true);
+
+        innerSideSplitPanel.getChildren().addAll(avatarSelection, avatarIV);
+        sidePanel.getChildren().addAll(avatarCenter, innerSideSplitPanel);
+
+        splitPanel.getChildren().addAll(inputFieldsPanel, sidePanel);
+
+        getChildren().add(splitPanel);
     }
 
     private class ConnectButtonHandler implements EventHandler<ActionEvent>{
